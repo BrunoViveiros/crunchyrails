cef5e486da56c0dd998584219600cce076eca76b
Marcio Flavio <mflaviof1995@gmail.com>
2024-04-29 09:49:54 -0300

    feat: navbar styles

--- src/components/molecules/Navbar/Navbar.tsx
+++ src/components/molecules/Navbar/Navbar.tsx
@@ -1,23 +1,37 @@
+import { Link } from "@/lib/Components/Link/Link";
 import styles from "./styles/Navbar.module.css";
+import { Image } from "@/lib/Components/Image/Image";
 
 const Navbar = () => {
   return (
-    <div className={styles.container}>
-      <nav className={styles.navbar}>
-        <div className={styles.logo}>Logo</div>
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Image src="/logo.svg" alt="logo" width={50} height={50} />
+      </div>
 
-        <ul className={styles.listContainer}>
-          <li>Search</li>
-          <li>Social</li>
-          <li>Forum</li>
-        </ul>
+      <ul className={styles.listContainer}>
+        <li>
+          <Link href="/" className={styles.listLink}>
+            Search
+          </Link>
+        </li>
+        <li>
+          <Link href="/" className={styles.listLink}>
+            Social
+          </Link>
+        </li>
+        <li>
+          <Link href="/" className={styles.listLink}>
+            Forum
+          </Link>
+        </li>
+      </ul>
 
-        <div className={styles.buttonContainer}>
-          <button>Login</button>
-          <button>Sign Up</button>
-        </div>
-      </nav>
-    </div>
+      <div className={styles.buttonContainer}>
+        <button className={styles.loginButton}>Login</button>
+        <button className={styles.signUpButton}>Sign Up</button>
+      </div>
+    </nav>
   );
 };
 
